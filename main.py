94bd110f81ba92dbcd69a484e8fa84c07b4b63e0
AxcelcampKanta <axcelcamp.kanta@gmail.com>
2021-09-28 18:30:02 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+screen.fill((0, 0, 0))
+pygame.display.set_caption("この世は、終わりだインベーダー")
+img = pygame.image.load("player.png")
+x = 200
+y = 200
+
+running = True
+while running:
+    screen.blit(img, (x, y))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                screen.fill((255,0,0))
+            if event.key == K_LEFT:
+                screen.fill((0,255,0))
+            if event.key == K_UP:
+                screen.fill((0,0,255))
+            if event.key == K_DOWN:
+                screen.fill((0,0,0))
+
+
+
+    pygame.display.update()
+
+
+
+
